3b4b09d7d128e8689223285f5ab48216420cce6d
jxxrd4n <davidilaquiche0@gmail.com>
2026-07-10 09:50:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-// Código JavaScript
+document.getElementById("startButton").addEventListener("click", function(){
+
+    alert("Bienvenida, mi bello copito de nieve 💖❄️\n\nNuestra historia apenas comienza...");
+
+});
